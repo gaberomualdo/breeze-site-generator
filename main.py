9032c1396c73b28abaf0d976804dee32dbd6a35b
xtrp <36894883+xtrp@users.noreply.github.com>
2018-12-28 19:46:35 +0000

    updated demo and split project files into valid code and embedded Python code

--- main.py
+++ main.py
@@ -31,6 +31,24 @@ for dirname, dirnames, filenames in os.walk(project_directory):
         if(valid_dir):
             project_files.append(os.path.join(dirname, filename))
 
+# Loop through project files and split into valid code and embedded Python code
+for file in project_files:
+    # Read and store file contents
+    file_contents = open(file, "r").read()
+
+    # Loop through contents and split into valid and embedded Python code; with even indices of split_file_contents array being valid code, and odd indices being Python embedded code
+    split_file_contents = [""]
+    for char_index in range(len(file_contents)):
+        char = file_contents[char_index]
+
+        # add new index to array if start or end embed tag has been found; if not, add content to last index in split contents list
+        if((char == "<" and file_contents[char_index+1] == "=" and len(split_file_contents)%2 == 1) or (char == "=" and file_contents[char_index+1] == ">" and len(split_file_contents)%2 == 0)):
+            split_file_contents.append("")
+        else:
+            # Only add char if char isn't end of embed tag; e.g. the ">" at the end of "=>" or the "=" at the end of "<="
+            if(not (split_file_contents[-1] == "" and ((char == "=" and file_contents[char_index-1] == "<") or (char == ">" and file_contents[char_index-1] == "=")))):
+                split_file_contents[-1] += char
+
 # === Just some code I'm saving for later :) ===
 # printVal = ""
 # def display(text):
