e3a4ff5e418ba3dec426813c702ea3d75b117ad0
xtrp <36894883+xtrp@users.noreply.github.com>
2018-12-28 18:46:04 +0000

    started Breeze

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+# (c) Breeze Static-Site Generator 2018
+# Made by Fred Adams (xtrp)
+# Check out Breeze on GitHub at https://github.com/xtrp/breeze
+# Check out the Breeze official website at https://xtrp.github.io/breeze/
+
+import sys
+import os
+
+# Get project directory file using command line argument
+try:
+    global project_directory
+    project_directory = sys.argv[1]
+except:
+    print("ERROR: valid project folder not specified")
+    sys.exit(1)
+
+# Get all files in project directory
+
+
 printVal = ""
 def display(text):
     global printVal
     if(printVal != ""):
         printVal += "\n"
-    printVal += text
+    printVal += str(text)
 
 exec('''
 global printVal
 printVal = ""
-display("test")
 ''')
 print printVal
